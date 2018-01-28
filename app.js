dd3ab1b0e3e9c89fa80f0c96ee46ff3a47acb8d4
David Valachovic <David.E.Valachovic@gmail.com>
2018-01-27 23:36:04 -0600

    client advances steps on its own

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const co = require('co')
 const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(bodyParser.json())
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -16,11 +15,12 @@ app.use(function (req, res, next) {
 let state = 0;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
 app.get('/state', (req, res) => res.sendFile())
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', function () {
+    socket.on('disconnect', () => {
         console.log('user disconnected');
     });
     socket.emit('state', state)
@@ -28,19 +28,10 @@ io.on('connection', function (socket) {
 
 http.listen(80, () => console.log('yognar listening on port 80!'))
 
-co(function* () {
-    while (true) {
-        yield waitSeconds(.5);
-        io.emit('doStep');
-        doStep()
-    }
-})
-
-function waitSeconds(secondsToWait) {
-    return new Promise(resolve => setTimeout(resolve, secondsToWait * 1000))
-}
+setInterval(doStep, 400)
 
 function doStep() {
+    //io.emit('doStep');
     if (state < 2) {
         state++
     } else {
